e799fedab578b99588f8a46a01b213bc7e89ff29
Divine Higgins <dhiggins94@gmail.com>
2023-04-24 11:19:47 -0400

    trying to deploy on vercel

--- server/index.js
+++ server/index.js
@@ -1,3 +1,43 @@
+// const express = require("express");
+// const app = express();
+// const http = require("http");
+// const cors = require("cors");
+// const { Server } = require("socket.io");
+// app.use(cors());
+
+// const server = http.createServer(app);
+
+// const io = new Server(server, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
+
+// io.on("connection", (socket) => {
+//   console.log(`User Connected: ${socket.id}`);
+//   // socket id stands for the user themselves in this case
+
+//   socket.on("join_room", (data) => {
+//     socket.join(data);
+//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+//   });
+//   // this connects to front end "join_room" this allows data to be accepted on the backend.
+
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//   });
+
+//   socket.on("disconnect", () => {
+//     console.log("User Disconnected", socket.id);
+//   });
+//   // this is so we can disconnect for our socker.io server, it'll run anytime soneone closes their connection to the server.
+// });
+
+// server.listen(3001, () => {
+//   console.log("SERVER RUNNING");
+// });
+
 const express = require("express");
 const app = express();
 const http = require("http");
@@ -15,13 +55,22 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
+    console.log("User Disconnected", socket.id);
   });
 });
-// this is so we can disconnect for our socker.io server, it'll run anytime soneone closes their connection to the server.
+
 server.listen(3001, () => {
-  console.log("server is live");
+  console.log("SERVER RUNNING");
 });
